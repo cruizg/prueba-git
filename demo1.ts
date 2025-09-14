b0df32d21b2fe5d1d309c3015f4552a96ac3d13d
cruizg <ruizgonzaleschristian@gmail.com>
2025-09-14 10:36:51 -0500

    se agregaron cambios iniciales

---
+++ demo1.ts
@@ -0,0 +1,29 @@
+
+    // Tipos
+    const batman = 'Bruce';
+    const superman = 'Clark';
+  
+    const existe = false;
+  
+    // Tuplas
+    const parejaHeroes = [batman,superman];
+    const villano = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+  
+    // Retorno de funciones
+    function activar_batiseñal(){
+      return 'activada';
+    }
+  
+    function pedir_ayuda(){
+      console.log('Auxilio!!!');
+    }
+  
+  
+  
+
+  
+  
